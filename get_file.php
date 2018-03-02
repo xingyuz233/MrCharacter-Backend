f70624bc7d762c74bcbddee84c1416b45e959e47
Garlic Xu <spurs-xu@qq.com>
2018-03-02 15:03:49 +0800

    Modified get_file.php to accept font and pic names

--- get_file.php
+++ get_file.php
@@ -1,11 +1,14 @@
 <?php
+
 session_start();
 
 if (is_uploaded_file($_FILES['bill']['tmp_name'])) {
-    $uploads_dir = './';
+    $uploads_dir = './' . $_SESSION['User'] . '/' . $_POST['font_name'] . '/';
+    if (!file_exists($uploads_dir))
+        mkdir($uploads_dir, 0777, true);
+
     $tmp_name = $_FILES['bill']['tmp_name'];
-    $pic_name = $_FILES['bill']['name'];
-    move_uploaded_file($tmp_name, $uploads_dir . $_SESSION['User'] . $pic_name);
+    move_uploaded_file($tmp_name, $uploads_dir . $_POST['pic_name']);
 } else {
     echo "File not uploaded successfully.";
 }
