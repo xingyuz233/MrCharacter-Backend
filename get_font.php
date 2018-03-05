d84fa3a2b857baaacd857ef893f20aa38fd71a2b
flower <hn915882174@gmail.com>
2018-03-05 23:00:43 +0800

    add get_progress

--- get_font.php
+++ get_font.php
@@ -23,13 +23,45 @@ try {
     if (!$_SESSION['User']) {
         echo "Wrong";
     } else {
-        $fontArrayResult = $pdo->query("SELECT * FROM testapp.font WHERE userid=".$_SESSION["User"]);
+        $fontArrayResult = $pdo->query("SELECT * FROM testapp.font WHERE userphone=".$_SESSION["User"]);
         $fontArray = $fontArrayResult->fetchAll();
+
+        $json_string = request_json($fontArray, $_SESSION["User"]);
+        $progress_list = get_progress($json_string);
+
+        foreach ($fontArray as $single_font) {
+            $single_font["progress"] = $progress_list[$single_font['id']];
+        }
+
+
         echo json_encode(array_utf8_encode($fontArray));
     }
 
 } catch (PDOException $e) {
     echo $e->getMessage();
+}
+
+function request_json($fontArray, $user_id) {
+
+    $fontList = array();
+    foreach ($fontArray as $single_font) {
+        array_push($fontList, $single_font["id"]);
+    }
+
+    return json_encode(array('cmd'=>'progress', 'user_id'=>$user_id, 'font_id'=>$fontList));
+}
+
+function get_progress($json_string) {
+    $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    socket_connect($socket, "localhost", 9999);
+
+    socket_write($socket, $json_string, strlen($json_string));
+
+    $out = socket_read($socket, 2048);
+
+    $progress_list = json_decode($out);
+
+    return $progress_list;
 }
 
 
